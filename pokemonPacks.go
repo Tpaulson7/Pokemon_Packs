acbbe1c53c7b32e6f9f0add96cfa1f22c9cd2ea8
Tyler Paulson <tpaulson9977@gmail.com>
2024-09-13 14:10:34 -0700

    Start of the outline for pokemon pack simulators

---
+++ pokemonPacks.go
@@ -0,0 +1,20 @@
+package main
+
+// Transfer JSON to PostgreSQL
+type Card struct {
+    Name  string `json:"name"`
+    Type  string `json:"type"`
+    Image string `json:"image"`
+}
+
+type CardData struct {
+    Common   []Card `json:"common"`
+    Uncommon []Card `json:"uncommon"`
+    Rare     []Card `json:"rare"`
+    Holo     []Card `json:"holo"`
+    Energy   []Card `json:"energy"`
+}
+
+func main() {
+    
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+async function fetchPokemonCards() {
+    
+};
+
+
+function displayCards() {
+    
+};
