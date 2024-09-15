4ee83d847f6fc4913ae232ce087d54a0175759ed
Tyler Paulson <tpaulson9977@gmail.com>
2024-09-15 12:25:46 -0700

    restructured

--- pokemonPacks.go
+++ pokemonPacks.go
@@ -1,19 +1,12 @@
 package main
 
-// Transfer JSON to PostgreSQL
 type Card struct {
-    Name  string `json:"name"`
-    Type  string `json:"type"`
-    Image string `json:"image"`
-}
-
-type CardData struct {
-    Common   []Card `json:"common"`
-    Uncommon []Card `json:"uncommon"`
-    Rare     []Card `json:"rare"`
-    Energy   []Card `json:"energy"`
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	Image  string `json:"image"`
+	Rarity string `json:"rarity"`
 }
 
 func main() {
-    
+
 }
