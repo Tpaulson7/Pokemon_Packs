668f1a3e1de33e8a3a7fe774d86606c88b0c1a40
Tyler Paulson <tpaulson9977@gmail.com>
2024-12-21 14:19:36 -0800

    Redid Keys

--- Backend/firebaseConfig.js
+++ Backend/firebaseConfig.js
@@ -1,18 +1,20 @@
+require('dotenv').config();
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCif4cLOXX7FB3TQDcaH3A4I_CpoXztomo",
-  authDomain: "pokemoncards-14756.firebaseapp.com",
-  projectId: "pokemoncards-14756",
-  storageBucket: "pokemoncards-14756.appspot.com",
-  messagingSenderId: "378830456622",
-  appId: "1:378830456622:web:b50e31ea17e59a53551cbf",
-  measurementId: "G-H3CZ08JJM6",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
